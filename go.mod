3ea3ab19ad71c913e4be4417bb43bac982f9ea57
Tehhs <lltsamoa@gmail.com>
2025-10-12 15:54:45 +1100

    saving lipgloss stuff
    not a real commit just need to save this for another computer

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module github.com/Tehhs/tdr
 
-go 1.24.0
-
-toolchain go1.24.6
+go 1.24.6
 
 require (
 	github.com/antlr4-go/antlr/v4 v4.13.1
@@ -39,5 +37,4 @@ require (
 	github.com/tree-sitter/go-tree-sitter v0.25.0
 	github.com/tree-sitter/tree-sitter-go v0.23.4
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
-
 )
--- pkg/cli/components/tags.go
+++ pkg/cli/components/tags.go
@@ -1,23 +1,62 @@
 package components
 
 import (
+
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	// "github.com/charmbracelet/lipgloss/table"
 )
+
+type Tag struct {
+	TagName string
+	Amt     int
+}
 
 type TagsModel struct {
 	FileName string
+	Tags     []Tag
+	TagIndex int
 }
 
+var (
+	purple    = lipgloss.Color("99")
+	gray      = lipgloss.Color("245")
+	lightGray = lipgloss.Color("241")
+
+	cellStyle    = lipgloss.NewStyle().Padding(0, 1).Padding(1, 0)
+	oddRowStyle  = cellStyle.Foreground(gray)
+	evenRowStyle = cellStyle.Foreground(lightGray)
+)
+
 func (m TagsModel) View() string {
-	s := `
+	s := ""
 
-*****************************************
-*
-*		THIS IS THE TAGS VIEW (WIP)
-*
-*****************************************
+	
+	// amtPerRow := 2 
+	// rows := [][]string{}
+	// for i, item := range m.Tags { 
+	// 	rowIndex := i / amtPerRow
+	// 	colIndex := i % amtPerRow
+	// 	rowIndex[rowIndex][colIndex] = item.TagName
 
-`
+	// }
+
+	
+
+	// t := table.New().
+	// 	Border(lipgloss.HiddenBorder()).
+	// 	Width(80).
+	// 	StyleFunc(func(row, col int) lipgloss.Style {
+	// 		switch {
+	// 		case row%2 == 0:
+	// 			return evenRowStyle
+	// 		default:
+	// 			return oddRowStyle
+	// 		}
+	// 	})
+		// Rows(rows...)
+
+	// s += t.Render()
 
 	return s
 }
@@ -31,5 +70,13 @@ func (m TagsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func NewTagsModel() TagsModel {
-	return TagsModel{}
+
+	tagsModel := TagsModel{}
+
+	tagsModel.Tags = []Tag{
+		{TagName: "Tag1"},
+		{TagName: "Tag2"},
+		{TagName: "Tag3"},
+	}
+	return tagsModel
 }
